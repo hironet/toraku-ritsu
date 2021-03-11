bc04ac71f14ae28801dcf16e0a12183132e1c3d3
Hiro <hiroyuki1126@gmail.com>
2021-03-11 22:25:25 +0900

    テーブル行数の変更処理を削除。

--- index.php
+++ index.php
@@ -5,38 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <style>
-    .ctrl-button {
-      border-radius: 4px;
-      border-style: none;
-      color: white;
-      margin: 10px 10px;
-      padding: 0.8em 0.2em;
-      position: relative;
-      width: 80px;
-    }
-
-    .ctrl-button:hover {
-      opacity: 0.7;
-    }
-
-    .ctrl-button:active {
-      top: 4px;
-    }
-
-    #change-button {
-      background-color: #1e90ff;
-    }
-
-    #add-button {
-      background-color: #32cd32;
-    }
-
-    #del-button {
-      background-color: #ff8c00;
-    }
-
-    #reset-button {
-      background-color: #ff69b4;
+    /* 第1カラムに適用 */
+    .col156-align td:nth-of-type(1), td:nth-of-type(5), td:nth-of-type(6) {
+      text-align: right;
     }
 
     output.toraku::after {
@@ -47,79 +18,75 @@
 </head>
 <body>
   <h1>騰落率計算ツール</h1>
-  <?php
-  const MIN_FORM_NUM = 1;
-  const MAX_FORM_NUM = 30;
-  const DEFAULT_FORM_NUM = 4;
-
-  $default_form_num = DEFAULT_FORM_NUM;
-  $form_num = isset($_GET['form_num']) ? htmlspecialchars($_GET['form_num']) : $default_form_num;
-  $form_num = $form_num > MIN_FORM_NUM ? $form_num : MIN_FORM_NUM;
-  $form_num = $form_num < MAX_FORM_NUM ? $form_num : MAX_FORM_NUM;
-  $form_num_plus_one = $form_num + 1;
-  $form_num_minus_one = $form_num - 1;
-  ?>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-lg-auto">
-        <form method="GET" action="index.php">
-          <label>項目数：<input class="form_num" type="number" name="form_num" value="<?php print $form_num ?>" style="width: 50px;"></label>
-          <input id="change-button" class="ctrl-button" type="submit" value="変更">
-        </form>
-      </div>
-      <div class="col-auto col-lg-auto">
-        <div class="btn-group" role="group">
-          <form method="GET" action="index.php">
-            <input type="hidden" name="form_num" value="<?php print $form_num_plus_one ?>">
-            <input id="add-button" class="ctrl-button" type="submit" value="1行追加">
-          </form>
-          <form method="GET" action="index.php">
-            <input type="hidden" name="form_num" value="<?php print $form_num_minus_one ?>">
-            <input id="del-button" class="ctrl-button" type="submit" value="1行削除">
-          </form>
-          <form method="GET" action="index.php">
-            <input type="hidden" name="form_num" value="<?php print $default_form_num ?>">
-            <input id="reset-button" class="ctrl-button" type="submit" value="リセット">
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="row" style="text-align: center;">
-      <div class="col-6 col-lg-1 border">No</div>
-      <div class="col-6 col-lg-2 border">銘柄</div>
-      <div class="col-6 col-lg-2 border">変動前の価格</div>
-      <div class="col-6 col-lg-2 border">変動後の価格</div>
-      <div class="col-6 col-lg-2 border">変動幅</div>
-      <div class="col-6 col-lg-2 border">騰落率</div>
-      <div class="col-12 col-lg-1 border"></div>
-    </div>
-  <?php
-  function calc_form($no) {
-    print <<<CALC_FORM
+  <div class="table-responsive">
     <form onsubmit="return false;" oninput="
-      hendou.value = calc_hendou(Number(a.value), Number(b.value));
-      toraku.value = calc_toraku(Number(a.value), Number(b.value));
+      hendou_1.value = calc_hendou(Number(before_price_1.value), Number(after_price_1.value));
+      toraku_1.value = calc_toraku(Number(before_price_1.value), Number(after_price_1.value));
+      hendou_2.value = calc_hendou(Number(before_price_2.value), Number(after_price_2.value));
+      toraku_2.value = calc_toraku(Number(before_price_2.value), Number(after_price_2.value));
+      hendou_3.value = calc_hendou(Number(before_price_3.value), Number(after_price_3.value));
+      toraku_3.value = calc_toraku(Number(before_price_3.value), Number(after_price_3.value));
+      hendou_4.value = calc_hendou(Number(before_price_4.value), Number(after_price_4.value));
+      toraku_4.value = calc_toraku(Number(before_price_4.value), Number(after_price_4.value));
+      hendou_5.value = calc_hendou(Number(before_price_5.value), Number(after_price_5.value));
+      toraku_5.value = calc_toraku(Number(before_price_5.value), Number(after_price_5.value));
     ">
-    <div class="row">
-      <div class="col-6 col-lg-1 border" style="text-align: right;">{$no}</div>
-      <div class="col-6 col-lg-2 border"><input class="brand" type="text" style="width: 100%;"></div>
-      <div class="col-6 col-lg-2 border"><input class="price" type="number" name="a" style="width: 100%;"></div>
-      <div class="col-6 col-lg-2 border"><input class="price" type="number" name="b" style="width: 100%;"></div>
-      <div class="col-6 col-lg-2 border" style="text-align: right;"><output name="hendou"></output></div>
-      <div class="col-6 col-lg-2 border" style="text-align: right;"><output class="toraku" name="toraku"></output></div>
-      <div class="col-12 col-lg-1 border"><input type="reset" value="リセット"></div>
-    </div>
-  </form>
-CALC_FORM;
-  }
-
-  for ($i = 1; $i <= $form_num; $i++) {
-    calc_form($i);
-  }
-  ?>
-  </div>
+      <table class="table table-striped table-bordered align-middle col156-align">
+        <thead>
+          <tr class="text-center row-nowrap">
+            <th>No</th>
+            <th>銘柄</th>
+            <th>変動前の価格</th>
+            <th>変動後の価格</th>
+            <th style="width: 10%;">変動幅</th>
+            <th style="width: 10%;">変動率</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td><input class="form-control" type="text"></td>
+            <td><input class="form-control" type="number" name="before_price_1"></td>
+            <td><input class="form-control" type="number" name="after_price_1"></td>
+            <td><output name="hendou_1"></output></td>
+            <td><output class="toraku" name="toraku_1"></output></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><input class="form-control" type="text"></td>
+            <td><input class="form-control" type="number" name="before_price_2"></td>
+            <td><input class="form-control" type="number" name="after_price_2"></td>
+            <td><output name="hendou_2"></output></td>
+            <td><output class="toraku" name="toraku_2"></output></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td><input class="form-control" type="text"></td>
+            <td><input class="form-control" type="number" name="before_price_3"></td>
+            <td><input class="form-control" type="number" name="after_price_3"></td>
+            <td><output name="hendou_3"></output></td>
+            <td><output class="toraku" name="toraku_3"></output></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td><input class="form-control" type="text"></td>
+            <td><input class="form-control" type="number" name="before_price_4"></td>
+            <td><input class="form-control" type="number" name="after_price_4"></td>
+            <td><output name="hendou_4"></output></td>
+            <td><output class="toraku" name="toraku_4"></output></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td><input class="form-control" type="text"></td>
+            <td><input class="form-control" type="number" name="before_price_5"></td>
+            <td><input class="form-control" type="number" name="after_price_5"></td>
+            <td><output name="hendou_5"></output></td>
+            <td><output class="toraku" name="toraku_5"></output></td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  </div><!-- table-responsive -->
   <script>
     function calc_hendou(a, b) {
       return Math.round((b - a) * 100) / 100;
